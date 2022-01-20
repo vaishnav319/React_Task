3201b9d7233a05b6383b47c39fd7dc70376e6708
vaishnav <msvsurya1@gmail.com>
2022-01-20 11:13:22 +0530

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Box from "./Box";
 import Spinner from "./Spinner";
 import { TextField } from "@mui/material";
-
+import store from "../store";
 import Order from "./Order";
-
+import { getPhotos } from "../actions/photos";
+import { getAlbums } from "../actions/albums";
 const Home = ({ photos: { photos }, albums: { albums, loading } }) => {
+  useEffect(() => {
+    store.dispatch(getPhotos());
+    store.dispatch(getAlbums());
+  }, []);
   return (
     <div>
-      <div style={{ margin: "1rem", width: "100%" }}>
-        <TextField
-          id="outlined-basic"
-          style={{ width: "100%" }}
-          label="Search Here"
-          variant="outlined"
-        />
-      </div>
-      <h2>{albums[0].title}</h2>
-      {/* <Order photos={photos} albums={albums} /> */}
-      {photos &&
-        photos.map((photo) => <Box style={{ width: "100%" }} photo={photo} />)}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div style={{ margin: "1rem", width: "100%" }}>
+          <TextField
+            id="outlined-basic"
+            style={{ width: "100%" }}
+            label="Search Here"
+            variant="outlined"
+          />
+          {photos &&
+            photos.map((photo) => (
+              <Box style={{ width: "100%" }} photo={photo} />
+            ))}
+        </div>
+      )}
     </div>
   );
 };
